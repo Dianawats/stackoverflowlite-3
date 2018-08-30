2e0cc30680084b06d8f9020bb5c857b9ed9a7ae8
Dianawats <dianakiznaki@gmail.com>
2018-08-30 03:59:41 +0300

    add db file

---
+++ app/api/db_manager/db_config.py
@@ -0,0 +1,27 @@
+import psycopg2
+import os
+from app.config import app_config
+
+from pprint import pprint
+
+
+class DatabaseConnection():
+
+    def __enter__(self):
+        try:
+            if app_config['testing']:
+                self.conn = psycopg2.connect("dbname = 'test_db' user = 'postgres' host = 'localhost' password = 'kekotowns' port = '5432'")         
+                self.cursor = self.conn.cursor()
+                return self.cursor
+            else:
+                self.conn = psycopg2.connect("dbname = 'stackoverlite' user = 'postgres' host = 'localhost' password = 'k3ko' port = '5432'")
+                self.cursor = self.conn.cursor()
+                return self.cursor
+        except (Exception, psycopg2.DatabaseError) as error:
+            pprint(error)
+    
+    def __exit__(self, exception_type, exception_val, exception_traceback):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+    
